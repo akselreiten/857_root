d94898ca5fe74db72af08c3f91e962dee05fb62b
aksel.reiten <aksel.reiten@gmail.com>
2016-04-16 18:17:11 -0400

    included web3

--- js/main.js
+++ js/main.js
@@ -8,3 +8,9 @@ console.log(arg);
 console.log("hey");
 */
 
+if(typeof web3 !== 'undefined')
+    web3 = new Web3(web3.currentProvider);
+else
+// set the provider you want from Web3.providers
+    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
