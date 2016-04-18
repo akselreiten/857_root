135535eb72ada90e326cd241106d278d5ff9e8e3
aksel.reiten <aksel.reiten@gmail.com>
2016-04-18 16:18:01 -0400

    minor changes to main.js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,6 @@
  * Created by akselreiten on 16/04/16.
  */
 
-/*  variables
-var arg = 2;
-console.log(arg);
-console.log("hey");
-*/
 
 if(typeof web3 !== 'undefined')
     web3 = new Web3(web3.currentProvider);
@@ -14,3 +9,18 @@ else
 // set the provider you want from Web3.providers
     web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
+
+//  Using Callbacks
+web3.eth.getBlock(48, function(error, result){
+    if(!error)
+        console.log(result)
+    else
+        console.error(error);
+})
+
+
+//  Batch requests
+var batch = web3.createBatch();
+batch.add(web3.eth.getBalance.request('0x0000000000000000000000000000000000000000', 'latest', callback));
+batch.add(web3.eth.contract(abi).at(address).balance.request(address, callback2));
+batch.execute();
