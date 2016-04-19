84859d16b3ca1ece957936febeb10c60309b8ac8
Francis Chen <fxchen12@gmail.com>
2016-04-19 13:15:18 -0400

    Added dep.

--- js/main.js
+++ js/main.js
@@ -8,3 +8,9 @@ console.log(arg);
 console.log("hey");
 */
 
+var Web3 = require('web3');
+var web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
+
+var con = web3.isConnected()
+console.log(con);
