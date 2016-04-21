8917a1bcae6de204fd1776baa0ce1be7357c527c
aksel.reiten <aksel.reiten@gmail.com>
2016-04-20 21:03:09 -0400

    Implemented the contract functions

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@
  * Created by akselreiten on 16/04/16.
  */
 
+var pkAksel = "0x3E1f6a79635dB65564DAEDB58E46EF75B3A0D898";
+var pkAksel2 = "0x67A6443f760866722ca0F44ae7dc55243b462Db2";
+var pkAksel3 = "0x1f82dE5CCEE343E0D663e2E1D82D69642362F736";
+var pkAllan = "0x70a5C2DC9abFAf62AfF603D1ba99f3A9032841d4";
+var pkKristoffer = "0x183fBACe28e683d8b1632777eFadB34E6d357d7E";
+var pkFrancis = "0xCd6a2d83699444203A172FcB6EFFb611e00206DC";
+
 //  Instantiating web3
 var Web3 = require('web3');
 var web3 = new Web3();
@@ -12,39 +19,115 @@ var eth = web3.eth;
 var personal = web3.personal;
 
 //  Set default account
-web3.eth.defaultAccount = "0x3E1f6a79635dB65564DAEDB58E46EF75B3A0D898";
+web3.eth.defaultAccount = pkFrancis;
 var defaultAccount = web3.eth.defaultAccount;
-
-console.log(eth.accounts);
-
-var con = web3.isConnected();
-console.log("connected:" +con);
 
 var address = "0xfAa51987F509f2dEeE58918E18d9A42aB999A3C2";
 
-console.log(web3.personal);
+var myContract = web3.eth.contract(abi);
+var myContractInstance = myContract.at(address);
+var result = myContractInstance.getBalance.call(pkFrancis).toNumber();
 
+//  Gets all requests and returns array
+function getRequests(){
+    var requestsIDs = myContractInstance.getAllRequests.call();
+    var requestArray = [];
+    requestsIDs.forEach(function(d){
+        var curr_request = myContractInstance.getRequest.call(d);
+        var request = new Object();
+        request.public_key = curr_request[0];
+        request.id = d;
+        request.amount = curr_request[1].toNumber();
+        request.duration = curr_request[2].toNumber();
+        request.bonus = curr_request[3].toNumber();
+        request.name = curr_request[4];
+        requestArray.push(request);
+    })
+    return requestArray;
+}
 
+console.log(getRequests());
+console.log(issueRequest(12,2,3,"aks2",pkAksel3));
 
+//  (Returns Boolean) Fulfil a request given the ID of the request
+function fulfillRequest(id,fromAddress){
+    if (myContractInstance.fulfillRequest.call(id,{from:fromAddress})){
+        var gas = myContractInstance.fulfillRequest.estimateGas(id);
+        myContractInstance.fulfillRequest.sendTransaction(id,{from:fromAddress, gas:gas*2});
+        return true;
+    }return false;
+}
 
-/*  Defining contracts
-var address = "0x3E1f6a79635dB65564DAEDB58E46EF75B3A0D898";
-//var microCoin = eth.contract([{ "constant": true, "inputs": [], "name": "name", "outputs": [ { "name": "", "type": "string", "value": "MicroCoin", "displayName": "" } ], "type": "function", "displayName": "name" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [ { "name": "", "type": "uint256", "value": "20", "displayName": "" } ], "type": "function", "displayName": "total Supply" }, { "constant": false, "inputs": [ { "name": "_from", "type": "address", "index": 0, "typeShort": "address", "bits": "", "displayName": "&thinsp;<span class=\"punctuation\">_</span>&thinsp;from", "template": "elements_input_address" }, { "name": "_to", "type": "address", "index": 1, "typeShort": "address", "bits": "", "displayName": "&thinsp;<span class=\"punctuation\">_</span>&thinsp;to", "template": "elements_input_address" }, { "name": "_value", "type": "uint256", "index": 2, "typeShort": "uint", "bits": "256", "displayName": "&thinsp;<span class=\"punctuation\">_</span>&thinsp;value", "template": "elements_input_uint" } ], "name": "transferFrom", "outputs": [ { "name": "success", "type": "bool" } ], "type": "function", "displayName": "transfer From" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [ { "name": "", "type": "uint8", "value": "10", "displayName": "" } ], "type": "function", "displayName": "decimals" }, { "constant": true, "inputs": [], "name": "version", "outputs": [ { "name": "", "type": "string", "value": "1.0", "displayName": "" } ], "type": "function", "displayName": "version" }, { "constant": true, "inputs": [ { "name": "", "type": "address", "index": 0, "typeShort": "address", "bits": "", "displayName": "", "template": "elements_input_address" } ], "name": "balanceOf", "outputs": [ { "name": "", "type": "uint256", "value": "0", "displayName": "" } ], "type": "function", "displayName": "balance Of" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [ { "name": "", "type": "string", "value": "MC", "displayName": "" } ], "type": "function", "displayName": "symbol" }, { "constant": false, "inputs": [ { "name": "_to", "type": "address", "index": 0, "typeShort": "address", "bits": "", "displayName": "&thinsp;<span class=\"punctuation\">_</span>&thinsp;to", "template": "elements_input_address" }, { "name": "_value", "type": "uint256", "index": 1, "typeShort": "uint", "bits": "256", "displayName": "&thinsp;<span class=\"punctuation\">_</span>&thinsp;value", "template": "elements_input_uint" } ], "name": "transfer", "outputs": [], "type": "function", "displayName": "transfer" }, { "constant": false, "inputs": [ { "name": "_spender", "type": "address", "index": 0, "typeShort": "address", "bits": "", "displayName": "&thinsp;<span class=\"punctuation\">_</span>&thinsp;spender", "template": "elements_input_address" }, { "name": "_value", "type": "uint256", "index": 1, "typeShort": "uint", "bits": "256", "displayName": "&thinsp;<span class=\"punctuation\">_</span>&thinsp;value", "template": "elements_input_uint" }, { "name": "_extraData", "type": "bytes", "index": 2, "typeShort": "bytes", "bits": "", "displayName": "&thinsp;<span class=\"punctuation\">_</span>&thinsp;extra Data", "template": "elements_input_bytes" } ], "name": "approveAndCall", "outputs": [ { "name": "success", "type": "bool" } ], "type": "function", "displayName": "approve And Call" }, { "constant": true, "inputs": [ { "name": "", "type": "address", "index": 0, "typeShort": "address", "bits": "", "displayName": "", "template": "elements_input_address" }, { "name": "", "type": "address", "index": 1, "typeShort": "address", "bits": "", "displayName": "", "template": "elements_input_address" } ], "name": "spentAllowance", "outputs": [ { "name": "", "type": "uint256", "value": "0", "displayName": "" } ], "type": "function", "displayName": "spent Allowance" }, { "constant": true, "inputs": [ { "name": "", "type": "address", "index": 0, "typeShort": "address", "bits": "", "displayName": "", "template": "elements_input_address" }, { "name": "", "type": "address", "index": 1, "typeShort": "address", "bits": "", "displayName": "", "template": "elements_input_address" } ], "name": "allowance", "outputs": [ { "name": "", "type": "uint256", "value": "0", "displayName": "" } ], "type": "function", "displayName": "allowance" }, { "inputs": [ { "name": "initialSupply", "type": "uint256", "index": 0, "typeShort": "uint", "bits": "256", "displayName": "initial Supply", "template": "elements_input_uint", "value": "20" }, { "name": "tokenName", "type": "string", "index": 1, "typeShort": "string", "bits": "", "displayName": "token Name", "template": "elements_input_string", "value": "MicroCoin" }, { "name": "decimalUnits", "type": "uint8", "index": 2, "typeShort": "uint", "bits": "8", "displayName": "decimal Units", "template": "elements_input_uint", "value": "10" }, { "name": "tokenSymbol", "type": "string", "index": 3, "typeShort": "string", "bits": "", "displayName": "token Symbol", "template": "elements_input_string", "value": "MC" }, { "name": "versionOfTheCode", "type": "string", "index": 4, "typeShort": "string", "bits": "", "displayName": "version Of The Code", "template": "elements_input_string", "value": "1.0" } ], "type": "constructor" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "from", "type": "address" }, { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" } ], "name": "Transfer", "type": "event" } ]);
-//var microCoinInstance = microCoin.at(addressMicroCoin);
-//var info = web3.getContractInfo(address);
-//var source = info.source;
-//var abiDef = info.abiDefinition;
-//console.log(abiDef);
+//  Issues a request for a loan (returns boolean)
+function issueRequest(amount,duration,bonus,name,fromAddress){
+    if (myContractInstance.issueRequest.call(amount,duration,bonus,name,{from:fromAddress})){
+        var gas = myContractInstance.issueRequest.estimateGas(amount,duration,bonus,name);
+        myContractInstance.issueRequest.sendTransaction(amount,duration,bonus,name,{from:fromAddress, gas:gas*2});
+        return true;
+    }return false;
+}
 
-//  Set default account
-web3.eth.defaultAccount = "0x3E1f6a79635dB65564DAEDB58E46EF75B3A0D898";
-var defaultAccount = web3.eth.defaultAccount;
+//  Payback function (returns boolean)
+function payback(id,fromAddress){
+    if (myContractInstance.payback.call(id,{from:fromAddress})){
+        var gas = myContractInstance.payback.estimateGas(id);
+        myContractInstance.payback.sendTransaction(id,{from:fromAddress, gas:gas*2});
+        return true;
+    }return false;
+}
 
-//  Get balance
-var balance = eth.getBalance("0x3E1f6a79635dB65564DAEDB58E46EF75B3A0D898")
-console.log(balance.toString());
+//  Triggers default on a loan (returns boolean)
+function trigger_default(id,fromAddress){
+    if (myContractInstance.trigger_default.call(id,{from:fromAddress})){
+        var gas = myContractInstance.trigger_default.estimateGas(id);
+        myContractInstance.trigger_default.sendTransaction(id,{from:fromAddress, gas:gas*2});
+        return true;
+    }return false;
+}
 
-//  Transfer money from Aksel to Kristoffer
-eth.sendTransaction({from: "0x3E1f6a79635dB65564DAEDB58E46EF75B3A0D898", to: "0x183fBACe28e683d8b1632777eFadB34E6d357d7E", value: web3.toWei(1,'ether')})
-*/
+//  Extends duration of a loan
+function extend(id,duration,fromAddress){
+    if (myContractInstance.extend.call(id,duration,{from:fromAddress})){
+        var gas = myContractInstance.extend.estimateGas(id,duration);
+        myContractInstance.extend.sendTransaction(id,duration,{from:fromAddress, gas:gas*2});
+        return true;
+    }return false;
+}
 
+console.log(getBalance(pkAksel));
+
+//  Gets balance of public key
+function getBalance(public_key) {
+    return myContractInstance.getBalance.call(public_key).toNumber();
+}
+
+//  Gets outstanding loans given a public key
+function getUserLoans(public_key){
+    var loans = myContractInstance.getOutstandingLoans.call(public_key);
+    var borrowed = [];
+    var lent = [];
+    loans.forEach(function(d){
+        var curr_loan = myContractInstance.getSingleLoan.call(d);
+        var loan = new Object();
+        loan.lender = curr_loan[0];
+        loan.borrower = curr_loan[1];
+        loan.amount = curr_loan[2];
+        loan.end_time = curr_loan[3];
+        loan.bonus = curr_loan[4];
+        loan.name = curr_loan[5];
+        loan.id = d;
+        if (loan.lender == public_key){lent.push(loan);}
+        else{borrowed.push(loan);}
+    });
+
+    var userLoan = new Object();
+    userLoan.borrowed = borrowed;
+    userLoan.lent = lent;
+
+    return userLoan;
+}
+
+//  Looks up user by public key and returns name as string
+function lookupPublicKey(public_key){
+    return myContractInstance.getName.call(public_key);
+}
